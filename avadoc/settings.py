762802c4ca50132642089ae3be1b131cca4f46e3
Rogério <rogerio.pereira@ifto.edu.br>
2023-12-16 22:13:35 -0300

    versão 5

--- avadoc/settings.py
+++ avadoc/settings.py
@@ -138,4 +138,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_REDIRECT_URL ='home'
 LOGIN_URL='login'
-LOGOUT_REDIRECT_URL='home'
+LOGOUT_REDIRECT_URL='login'
--- usuarios/urls.py
+++ usuarios/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(
         template_name='usuarios/login.html'
     ), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(template_name='avadoc/home.html'), name='logout'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='usuarios/login.html'), name='logout'),
 
     path('criar_usuario/', criar_usuario,name='criar_usuario'),
 ]
